f7c9c0147b6263620a9fc92220b7eba2ef48438c
gautamsen117 <gautamsen117@gmail.com>
2022-09-10 01:20:46 +0600

    conversion

---
+++ conversion.js
@@ -0,0 +1,38 @@
+var price1 = 31;
+var price2 = 10;
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+
+var price1 = '31';
+var price2 = '10';
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+var price1 = 31;
+var price2 = '10';
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+var price1 = '31';
+var price2 = 10;
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+// convert string to number
+
+var age1 = '31';
+var age2 = 10;
+var totalAge = age1 + age2;
+// console.log(totalAge);
+
+// Convert
+
+// console.log(age1);
+age1 = parseInt(age1);
+// console.log(age1);
+
+
+// convert float string to number 
+var gpa = '3.33';
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
