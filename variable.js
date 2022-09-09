f9e28db5e35a24f8f68f535fecad915bcbff4d3c
gautamsen117 <gautamsen117@gmail.com>
2022-09-09 22:48:08 +0600

    added variable

--- variable.js
+++ variable.js
@@ -1,7 +1,22 @@
 // vary + able = variable
+
+// numeric data types:
+
 var price = 11;
 var age = 18;
 var temparature = 38;
 var weight = 49;
 var money = 28;
 var quentity = 2;
+
+// string data types:
+
+var person = "Gautam Sen";
+var location = "Gulsha Bangladesh";
+var special = 'javascript language';
+
+// Boolean data types
+
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
