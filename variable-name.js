882a618e9cab71c585a61c7e8bfab8be2ab9fee4
gautamsen117 <gautamsen117@gmail.com>
2022-09-09 23:14:38 +0600

    variable name

---
+++ variable-name.js
@@ -0,0 +1,21 @@
+// good variable
+var price = 18;
+//1. variable name can not be any keywords
+// *Example: var for = "something"; wrong
+var name = "something";
+//2. variable name has to be in one word. No space
+// *Example: var person name = "web developer"; wrong
+var personname = "web developer";
+//3. variable name can not have quotion
+// *Example: var "person" = "some one"; wrong
+var person = "some one"; right
+//4. variable name can not starts with a number but can ends with a number
+// *Example: var 33person = "programmer"; wrong
+var person33 = "programmer";
+//5. How to use long names
+// can not use dash but use underscore (_)
+var userHomeAddress = "Washington DC"; // Camel Case
+var userHomeAddress = "Washington DC"; //Pascal Case
+var user_home_address = "Washington DC"; // Snake Case
+//6. Keyword Case Senstive
+//7. variable name is case senstive
