391a0a185c9c8b8748f933d2ecc9b2d7e89d93c5
gautamsen117 <gautamsen117@gmail.com>
2022-09-10 00:40:40 +0600

    adding shorthand

---
+++ shorthand.js
@@ -0,0 +1,43 @@
+// variable declaration
+var tomatoPrice = 25;
+// variable value update
+tomatoPrice = 30;
+
+// add something to previous value
+var price1 = 31;
+var price2 = 10;
+
+// set a new value 
+// price1 = 35;
+
+// add some value to the existing value
+price1 = price1 + 10;
+// console.log(price1);
+
+// addition to the same variable shorthand
+// += means adding some value to the existing value
+price1 += 10;
+
+price2 = price2 - 5;
+// console.log(price2);
+
+//shorthand
+// -= means diducting some values from the current value of the variable
+price2 -= 5;
+
+// *=
+price1 *= 10;
+
+// /=
+price1 /= 5;
+
+// ++ means adding 1 to the exixting 
+var age = 15;
+age = age +1;
+console.log(age);
+age++;
+
+// -- means you are diducting 1 from the existing value
+var love = 100;
+love = love -1;
+love--;
