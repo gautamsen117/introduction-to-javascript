c07747d71386f427d3eb41b2b3fa2f33e2474837
gautamsen117 <gautamsen117@gmail.com>
2022-09-10 16:13:12 +0600

    practice problem three

---
+++ practice-problem-three.js
@@ -0,0 +1,9 @@
+// John's teacher gave two variable
+var firstSample = "I am going to School";
+var secondSample = "an awesome web developer";
+// now i will try to bring one line two variable
+var fullSample = firstSample + secondSample;
+console.log(fullSample);
+// Now I will try to give a space in between two string
+fullSample = firstSample + " " + secondSample;
+console.log(fullSample);
