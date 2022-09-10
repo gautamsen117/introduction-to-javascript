7bf271fb70ec53781d80bab0e0631a38646a65a4
gautamsen117 <gautamsen117@gmail.com>
2022-09-10 16:22:54 +0600

    practice problem four

---
+++ practice-problem-four.js
@@ -0,0 +1,6 @@
+// Sarah's mother gave a number
+var mathNumber = 119;
+var dividedNumber = 5;
+// Now i will try  prit the remainder number
+var remainderNumber = mathNumber % dividedNumber;
+console.log(remainderNumber);
