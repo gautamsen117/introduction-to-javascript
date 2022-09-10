8937e829fb541f209edb90fd5f4b014db5c981ea
gautamsen117 <gautamsen117@gmail.com>
2022-09-10 16:05:18 +0600

    practice problem two

--- practice-problem-two.js
+++ practice-problem-two.js
@@ -3,5 +3,15 @@ var biologyMarks = 65;
 var chemistryMarks = 80;
 var physicsMarks = 35.45;
 var banglaMarks = 99.50;
+// total marks
 var totalMarks = mathMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks;
 console.log(totalMarks);
+// average marks
+var averageMarks = totalMarks / 5;
+console.log(averageMarks);
+// now print two decimal places
+averageMarks = averageMarks.toFixed(2);
+console.log(averageMarks);
+// averageMarks string to number
+averageMarks = parseFloat(averageMarks);
+console.log(averageMarks);
